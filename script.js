bccd3b90280c3dd9fef3132c866282d131c404bc
returntrip444 <danp777991@gmail.com>
2020-04-24 09:54:08 -0700

    finished code

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ var upperCase=["A","S","D","C","F","J","E","R","W","L","V","X","N","Q","P","Y","
 var numbers=["1","2","3","4","5","6","7","8","9","0"];
 var specialCharAt=["!","@","#","$","%","^","&","*","(",")"];
 var passwordLength=["how many characters would you like your password to be? minimum 8 characters max 120 characters"]
-
+var availableCharAt=[]
 
 
 function writePassword() {
+  
+  var finalPassword=generatePassword();
   var passwordText = document.querySelector('#password');
-  // var specialCharAt = indexOf(specialChar);
-  // var lowerCase = indexOf(lowerCase);
-  // var upperCase = indexOf(upperCase);
-  // var numbers = indexOf(numbers);
+   passwordText.textContent=finalPassword
+
+}
   
-  generatePassword();
+ 
   
-  // (Math.floor(Math.random() * Math.floor())
+  
 
   function createAvailableOptions() {
     
@@ -29,31 +30,78 @@ function writePassword() {
   
 
   function generatePassword(password){
+
+    
+
+    //add another confirm to determine password length
+
+    var passwordLength = prompt("how long would you like your password to be?")
+
+  
+ 
+    //set password length equal to variable
+ 
+
+
     var isLowerCase = confirm("Would you like to use lowercase letter?")
     console.log(isLowerCase)
-    if(isLowerCase === true) {
-      alert("yes its true")
+    if(isLowerCase === true) { 
+      for (var i=0; i < lowerCase.length; i++) {
+        availableCharAt.push(lowerCase[i])
+      }
+      
     }
 
     var isUpperCase = confirm("would you like to use uppercase letters?")
     console.log(isUpperCase)
     if(isUpperCase === true) {
-      alert ("yes its true")
+      for (var i=0; i < upperCase.length; i++) {
+        availableCharAt.push(upperCase[i])
+      }
+     
     }
 
     var isNumbers = confirm("would you like to use numbers?")
     console.log(isNumbers)
     if(isNumbers === true) {
-      alert ("yes its true")
+      for (var i=0; i < numbers.length; i++) {
+        availableCharAt.push(numbers[i])
+      }
+     
     }
     var isSpecialCharAt = confirm("would you like to use special characters?")
     console.log(isSpecialCharAt)
     if(isSpecialCharAt === true){
-      alert ("yes its true")
+      for (var i=0; i < specialCharAt.length; i++) {
+        availableCharAt.push(specialCharAt[i])
+     
     }
+  }
+    var finalPassword ="";
 
+     finalPassword+=availableCharAt[1]  
+     for (var i=0; i < finalPassword.length; i++) {
+      availableCharAt.push(finalPassword[i])
+    
+     }
+
+// loop through available char array the number of times the user requested the password length to be
+   var finalPassword ="";
+   for (var i=0; i < passwordLength; i++) {
+    finalPassword += availableCharAt[(Math.floor(Math.random() * (availableCharAt.length -1) ))]
+    
+     
+   }
+
+  return finalPassword
+
+  }
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
+
+// function generate(Math.floor(Math.random() * Math.floor())
+
+// return finalPassword
